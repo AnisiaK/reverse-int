e0ea623065655a0c7a45a6676e1f616a3c8bd0bd
Anisiia <anisia.kurenova@gmail.com>
2023-08-20 15:07:11 +0100

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function reverse (n) {
-  
+    if (n<0){
+        const num = n.toString().slice(1)
+        const arrayOfStrings = Array.from(num)
+  const arrayL = arrayOfStrings.length - 1;
+  const myArray = []
+  for (let i =arrayL; i>=0; i-- ){
+    myArray.push(arrayOfStrings[i])
+  }
+  let b = +myArray.join('')
+  return b
+    } else if (n>0){
+        const arrayOfStrings = Array.from(String(n))
+  const arrayL = arrayOfStrings.length - 1;
+  const myArray = []
+  for (let i =arrayL; i>=0; i-- ){
+    myArray.push(arrayOfStrings[i])
+  }
+  let b = +myArray.join('')
+  return (b)
+    } else if (n === 0){
+        return(n)
+    }
 }
